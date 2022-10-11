2a8a8764486a946ca41d5d44f8f749737ce15fe9
Amit Bar-Gil <amit217@yandex.com>
2022-10-11 17:58:37 +0300

    Test: GET: me, id, all | PATCH: me

--- back-end/e2e.test.js
+++ back-end/e2e.test.js
@@ -9,23 +9,23 @@ beforeAll(() => {
 
 afterAll((done) => {
   mongoose.connection.close();
+  mongoose.connection.dropDatabase();
   done();
 });
 
+let token;
+let id;
 describe('User', () => {
-  let token;
-  let id;
   const user = { email: 'valid@email.com', password: 'password' };
+  const newUser = { name: 'new', about: 'new' };
   it('Should return 201 status code for /signup', async () => {
     const res = await request.post('/signup').send(user);
     expect(res.status).toBe(201);
     expect(res.body.email).toBe(user.email);
-    return res;
   });
   it('Should return 400 status code for /signup', async () => {
     const res = await request.post('/signup').send(user.password);
     expect(res.status).toBe(400);
-    return res;
   });
   it('Should return 200 status code for /signin', async () => {
     const res = await request.post('/signin').send(user);
@@ -34,6 +34,38 @@ describe('User', () => {
     expect(res.body.token).toBeDefined();
     token = res.body.token;
     id = res.body.user._id;
-    return res;
+  });
+  it('Should return 200 status code with all the users for /users', async () => {
+    const res = await request
+      .get('/users')
+      .set('Authorization', 'Bearer ' + token);
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveLength(1);
+  });
+  it('Should return 200 status code with user body for /users:_id', async () => {
+    const res = await request
+      .get(`/users/${id}`)
+      .set('Authorization', 'Bearer ' + token);
+    expect(res.status).toBe(200);
+    expect(res.body.email).toBe(user.email);
+    expect(res.body.name).toBe('Jacques Cousteau');
+    expect(res.body.about).toBe('Explorer');
+  });
+  it('Should return 200 status code with updated user data for /users/me', async () => {
+    const res = await request
+      .patch('/users/me')
+      .set('Authorization', 'Bearer ' + token)
+      .send(newUser);
+    expect(res.status).toBe(200);
+    expect(res.body.name).toBe('new');
+    expect(res.body.about).toBe('new');
+  });
+  it('Should return 200 status code with current user by JWT for /users/me', async () => {
+    const res = await request
+      .get(`/users/me`)
+      .set('Authorization', 'Bearer ' + token);
+    expect(res.status).toBe(200);
+    expect(res.body.name).toBe('new');
+    expect(res.body.about).toBe('new');
   });
 });
